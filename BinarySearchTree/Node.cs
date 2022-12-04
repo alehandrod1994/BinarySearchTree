efd01662498232b4cd2abda8c7c1494cf7f9a039
alehandrod alehandrod <alehandrod@yandex.ru>
2022-12-04 15:34:40 +0700

    Добавьте файлы проекта.

---
+++ BinarySearchTree/Node.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    public class Node<T> : IComparable
+        where T : IComparable
+    {
+        public Node(T data)
+        {
+            Data = data;
+        }
+
+        public Node(T data, Node<T> left, Node<T> right)
+        {
+            Data = data;
+            Left = left;
+            Right = right;
+        }
+
+        public T Data { get; private set; }
+        public Node<T> Left { get; private set; }
+        public Node<T> Right { get; private set; }
+
+        public void Add(T data)
+        {
+            var node = new Node<T>(data);
+
+            if (node.Data.CompareTo(Data) == -1)
+            {
+                if (Left == null)
+                {
+                    Left = node;
+                }
+                else
+                {
+                    Left.Add(data);
+                }
+            }
+            else
+            {
+                if (Right == null)
+                {
+                    Right = node;
+                }
+                else
+                {
+                    Right.Add(data);
+                }
+            }
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj is Node<T> item)
+            {
+                return Data.CompareTo(item);
+            }
+            else
+            {
+                throw new ArgumentException("Несовпадение типов.");
+            }
+            
+        }
+
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+    }
+}
